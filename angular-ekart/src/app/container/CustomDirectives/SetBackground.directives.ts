45c92bee42125dd0cbe9c336a61c6a0b9670b154
Anoop Xminds <anoopak@xminds.com>
2024-06-11 15:01:39 +0530

    Use of Renderer2 in Angular

--- angular-ekart/src/app/container/CustomDirectives/SetBackground.directives.ts
+++ angular-ekart/src/app/container/CustomDirectives/SetBackground.directives.ts
@@ -1,16 +1,18 @@
-import { Directive, ElementRef, OnInit } from "@angular/core";
+import { Directive, ElementRef, OnInit, Renderer2 } from "@angular/core";
 
 @Directive({
     selector: '[setBackground]'
 })
 export class SetBackgound implements OnInit{
     //private element: ElementRef;
-    constructor(private element: ElementRef){
-        this.element = element;
+    constructor(private element: ElementRef, private renderer: Renderer2){
+        // this.element = element;
     }
 
     ngOnInit(){
-        this.element.nativeElement.style.backgroundColor = "#36454F";
-        this.element.nativeElement.style.color = "White";
+        // this.element.nativeElement.style.backgroundColor = "#36454F";
+        // this.element.nativeElement.style.color = "White";
+        this.renderer.setStyle(this.element.nativeElement, "backgroundColor", "#36454F");
+        this.renderer.setStyle(this.element.nativeElement, "color", "White");
     }
 }
