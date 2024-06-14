75510e4a7540ee62c4d9089a3de9ce9b97dfd35d
Anoop Xminds <anoopak@xminds.com>
2024-06-14 15:33:16 +0530

    Error & Completion of Observable

--- angular_observable/src/app/app.component.ts
+++ angular_observable/src/app/app.component.ts
@@ -19,15 +19,30 @@ export class AppComponent {
     setTimeout(() => {observer.next(1)}, 1000);
     setTimeout(() => {observer.next(2)}, 2000);
     setTimeout(() => {observer.next(3)}, 3000);
+    // setTimeout(() => {observer.error(new Error('Something went wrong. Please try later.!'))}, 3000);
     setTimeout(() => {observer.next(4)}, 4000);
     setTimeout(() => {observer.next(5)}, 5000);
+    setTimeout(() => {observer.complete()}, 6000);
   });
 
   getAsyncData(){
-      this.myObservable.subscribe((val: any) => {
-        this.data.push(val);
+      // this.myObservable.subscribe((val: any) => {
+      //   this.data.push(val);
+      // },(err) => {
+      //   alert(err.message);
+      // },() => {
+      //   alert("All the data is steamed..!")
+      // });
+
+      this.myObservable.subscribe({
+        next: (val: any ) => {
+          this.data.push(val);
+        },error(err){
+          alert(err.message);
+        },
+        complete(){
+          alert("All completed.!");
+        }
       });
   }
-
-
 }
