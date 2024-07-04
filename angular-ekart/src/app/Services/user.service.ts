726086dd7d6cb34602ebcc7267c8ee97f198128f
Anoop Xminds <anoopak@xminds.com>
2024-07-04 18:36:52 +0530

    Angular Router & Route Guards

---
+++ angular-ekart/src/app/Services/user.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import {User} from "../Models/User";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  users: User[] = [
+    new User(1, 'Anoop', 'anoopak@xminds.com', 'anoopxminds', 'anoop@xminds'),
+    new User(2, 'User 1', 'user1@xminds.com', 'user1xminds', 'user1@xminds'),
+    new User(3, 'User 2', 'user2@xminds.com', 'user2xminds', 'user2@xminds'),
+    new User(4, 'User 3', 'user3@xminds.com', 'user3xminds', 'user3@xminds')
+  ];
+}
--- angular-ekart/src/app/app.module.ts
+++ angular-ekart/src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 
 import { HeaderComponent } from './header/header-component';
-import { TopHeaderComponent } from './top-header/top-header.component';
+import { TopHeaderComponent } from './header/top-header/top-header.component';
 import { TopMenuComponent } from './header/top-menu/top-menu.component';
 import { MainMenuComponent } from './header/main-menu/main-menu.component';
 import { ContainerComponent } from './container/container.component';
@@ -15,6 +15,23 @@ import { ProductComponent } from './container/product-list/product/product.compo
 import { FilterComponent } from './container/product-list/filter/filter.component';
 import { ProductDetailComponent } from './container/product-detail/product-detail.component';
 import { FeaturedBrandsComponent } from './container/featured-brands/featured-brands.component';
+import {RouterModule, Routes} from "@angular/router";
+import {LoginComponent} from "./login/login.component";
+import {HomeComponent} from "./home/home.component";
+import {BannerComponent} from "./home/banner/banner.component";
+import {ProductsComponent} from "./products/products.component";
+import {FooterComponent} from "./footer/footer.component";
+import {ContactComponent} from "./contact/contact.component";
+import {NotFoundComponent} from "./not-found/not-found.component";
+
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'home', component: HomeComponent},
+  {path: 'products', component: ProductsComponent},
+  {path: 'contact', component: ContactComponent},
+  {path: 'login', component: LoginComponent},
+  {path: '**', component: NotFoundComponent}
+]
 
 @NgModule({
   declarations: [
@@ -29,12 +46,21 @@ import { FeaturedBrandsComponent } from './container/featured-brands/featured-br
     ProductComponent,
     FilterComponent,
     ProductDetailComponent,
-    FeaturedBrandsComponent
+    FeaturedBrandsComponent,
+    LoginComponent,
+    HomeComponent,
+    BannerComponent,
+    ProductsComponent,
+    FooterComponent,
+    ContactComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(routes)
   ],
+  exports:[RouterModule],
   providers: [],
   bootstrap: [AppComponent]
 })
